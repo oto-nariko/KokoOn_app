45f7b64c14e3dfeae9d7e6eccc1ba49b655c9fc1
kotoka <fko2547055@stu.o-hara.ac.jp>
2025-12-07 21:58:48 +0900

    コメントのデータベースを追加したとこ

--- KokoOnproject/KokoOn/admin.py
+++ KokoOnproject/KokoOn/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category, MoodPost
+from .models import Category, MoodPost, Comment
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'title')
@@ -9,5 +9,17 @@ class MoodPostAdmin(admin.ModelAdmin):
     list_display = ('id', 'title')
     list_display_links = ('id', 'title')
 
+class CommentAdmin(admin.ModelAdmin):
+    # 管理画面の一覧画面で表示するフィールド
+    list_display = ('user', 'mood', 'text', 'youtube_id', 'posted_at')
+    
+    # 管理画面の編集フォームで編集可能にするフィールド
+    # ここに 'youtube_id' を追加して、手動で確認・編集できるようにします。
+    fields = ('user', 'mood', 'text', 'youtube_id') 
+    
+    # 検索可能なフィールド
+    search_fields = ('user__username', 'text', 'youtube_id')
+
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(MoodPost, MoodPostAdmin)
+admin.site.register(Comment, CommentAdmin)
--- KokoOnproject/KokoOn/models.py
+++ KokoOnproject/KokoOn/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from accounts.models import CustomUser
+import re
 
 class Category(models.Model):
     '''
@@ -44,3 +45,53 @@ class MoodPost(models.Model):
     def __str__(self):
         return self.title
     
+class Comment(models.Model):
+    user = models.ForeignKey(
+        CustomUser,
+        verbose_name='ユーザー',
+        on_delete=models.CASCADE
+    )
+
+    mood = models.ForeignKey(
+        'MoodPost', # 同じファイル内にあるMoodPostモデルを参照
+        verbose_name='対象投稿',
+        on_delete=models.CASCADE,
+        related_name='comments_for_post'
+    )
+    
+    text = models.TextField(verbose_name="コメント内容")
+
+    posted_at = models.DateTimeField(
+        verbose_name='投稿日時',
+        auto_now_add=True
+    )
+
+    youtube_id = models.CharField(max_length=20, null=True, blank=True)
+
+# YouTube URLの正規表現
+    # 例: https://www.youtube.com/watch?v=xxxxxxxxxxx や https://youtu.be/xxxxxxxxxxx に対応
+    YOUTUBE_RE = re.compile(
+        r'(?:https?://)?(?:www\.)?(?:youtube\.com/watch\?v=|youtu\.be/)([^& \n]+)'
+    )
+
+    def save(self, *args, **kwargs):
+        # コメントテキストからYouTube IDを抽出
+        match = self.YOUTUBE_RE.search(self.text)
+        if match:
+            self.youtube_id = match.group(1)
+        else:
+            self.youtube_id = None
+            
+        super().save(*args, **kwargs)
+
+    # コメントテキストからURLを取り除くメソッド (オプション)
+    def clean_text(self):
+        return self.YOUTUBE_RE.sub('', self.text).strip()
+
+    # 動画が存在するか確認するプロパティ (テンプレートで使う)
+    @property
+    def has_youtube(self):
+        return self.youtube_id is not None and self.youtube_id != ''
+    
+    def __str__(self):
+        return f'{self.user.username} - {self.mood.title[:10]}...'
